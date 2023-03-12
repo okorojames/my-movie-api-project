dc079502a173a48ef39b8029e54374370f88a176
okorojames <okorojameschizaram@gmail.com>
2023-03-12 07:40:11 +0100

    fetched the data, still trying to use nee mthod to fetch it, to compare more efficient method

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
 import Header from "./components/Header";
 function App() {
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/550?api_key=296b046a3d7afb8c7d9de3d141e11353"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
   return (
     <main className="App">
       <Header />
